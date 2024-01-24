362f445893c7d8c99d1335ce2c78f428e2ff0995
hanjiawang <1301671498@qq.com>
2024-01-24 20:06:13 +0800

    feat: 使用vite编译jsx

--- App.js
+++
@@ -1,6 +0,0 @@
-import React from './core/React.js'
-import ReactDom from './core/ReactDom.js'
-
-const domEL = React.createElement('div', {id:'app'}, 'app', 'xxx', '-i')
-
-ReactDom.createRoot(document.querySelector('#root')).render(domEL)
