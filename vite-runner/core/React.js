3a5995ef4f9a8cc00ee8cf2ecf6d0c8851cacee1
hanjiawang <1301671498@qq.com>
2024-01-24 21:32:38 +0800

    feat: 实现简单fiber

--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -20,17 +20,83 @@ function createElement(type, props, ...children) {
   }
 }
 
-function render(el, container) {
-  const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type)
-  Object.keys(el.props).forEach(key => {
+let nextWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while(!shouldYield && nextWork) {
+    // 执行任务
+    nextWork = performWorkUnit(nextWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function creatDom(type) {
+  return type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(type)
+}
+
+function updateProps(dom, props) {
+  Object.keys(props).forEach(key => {
     if(key !== 'children') {
-      dom[key] = el.props[key]
+      dom[key] = props[key]
     }
   })
-  el.props.children.forEach(child => {
-    render(child, dom)
+}
+
+function initChildren(fiber) {
+  const children = fiber.props.children
+  let preChild = null
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null
+    }
+    if(index === 0) {
+      fiber.child = newFiber
+    }else {
+      preChild.sibling = newFiber
+    }
+    preChild = newFiber
   })
-  container.append(dom)
+}
+
+function performWorkUnit(fiber) {
+  console.log('fiber', fiber)
+  // 1. 创建dom
+  if(!fiber.dom) {
+    const dom = fiber.dom = creatDom(fiber.type)
+    fiber.parent.dom.append(dom)
+    // 2. 处理props
+    updateProps(dom, fiber.props)
+  }
+  // 3. 生成链表，创建指针
+  initChildren(fiber)
+  // 4. 返回下一个任务
+  if(fiber.child) {
+    return fiber.child
+  }
+  if(fiber.sibling) {
+    return fiber.sibling
+  }
+  return fiber?.parent?.sibling
+}
+
+
+
+function render(el, container) {
+  nextWork = {
+    dom: container,
+    props: {
+      children: [el]
+    }
+  }
 }
 
 const React = {
---
+++ vite-runner/index.js
@@ -0,0 +1,12 @@
+let taskId = 1
+function workLoop(deadline) {
+  ++taskId
+  let shouldYield = false
+  while(!shouldYield) {
+    console.log(`${taskId}`)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+// requestIdleCallback(workLoop)
