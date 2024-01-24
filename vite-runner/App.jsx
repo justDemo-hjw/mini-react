fce7a697351facbdf3eb01c2ea306e8c750bd00e
hanjiawang <1301671498@qq.com>
2024-01-24 20:05:25 +0800

    feat: 使用vite编译jsx

---
+++ vite-runner/App.jsx
@@ -0,0 +1,9 @@
+import React from './core/React.js'
+
+function AppOne() {
+  return <div id='dsadsa'>hi,dsadsa</div>
+}
+
+const App = <div id='222ds'>121</div>
+console.log(AppOne)
+export default App
---
+++ vite-runner/main.js
@@ -0,0 +1,6 @@
+import React from './core/React.js'
+import ReactDom from './core/ReactDom.js'
+import App from './App.jsx'
+
+console.log(App)
+ReactDom.createRoot(document.querySelector('#root')).render(App)
